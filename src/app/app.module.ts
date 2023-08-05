dd12272b6c5fddd3908348b549975b08f6f1f332
thenura442 <adalanane111@gmail.com>
2023-08-05 14:01:33 +0530

    home done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaceOrderComponent } from './pages/place-order/place-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceOrderComponent
   ],
   imports: [
     BrowserModule,
