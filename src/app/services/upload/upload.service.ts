00a768c4b9a65dee155a3a0eca0aaf75677a4e24
uasalam <umarsalam0011@gmail.com>
2023-08-17 13:22:54 +0530

    change

--- src/app/services/upload/upload.service.ts
+++ src/app/services/upload/upload.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const URL = 'http://localhost:4200/';
+const URL = 'http://localhost:5500/';
 const PATH = 'api/upload/';
 
 @Injectable({
